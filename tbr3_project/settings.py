b38c042b290d00b19bdcc83f41378d99014e3144
TonyxDz <tony23dz@gmail.com>
2022-01-12 10:40:00 +0100

    Remove secret key value from settings
    We get in from env variables for securety reasons

--- tbr3_project/settings.py
+++ tbr3_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-it*lql=3#1o8iss%_2id4b6-qhr@7nb0p$y6q$0m4l0x9gxoc-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
